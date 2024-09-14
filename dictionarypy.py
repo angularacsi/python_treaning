8065cc121a7c45b612898a7b07ff67ab94793b22
Walelign Balew <walelignbalew2013@gmail.com>
2024-09-14 12:34:40 +0300

    three

---
+++ dictionarypy.py
@@ -0,0 +1,22 @@
+#   Licensed under the Apache License,
+
+student={'Name':'walelig','age':'30','address':'Addis abeba','city':'addis','country':'ethiopia'}
+
+ 
+
+print (student['Name'])
+
+print(student.get('Addschool'))#none exists key value
+
+student['Name'] ='sharuk'
+
+print (student)
+
+ages=student.pop('age')
+
+#del student('address')
+
+print (student)
+
+print (ages)
+
